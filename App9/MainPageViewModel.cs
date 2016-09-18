459f16cfdeee2dc90a0a521ca165f01307cd7eb8
Charlie Pastre <charlie@pastre.com>
2016-09-18 13:36:18 -0700

    updated for keypad

--- App9/MainPageViewModel.cs
+++ App9/MainPageViewModel.cs
@@ -10,9 +10,9 @@ namespace App9
 {
     public class MainPageViewModel : BindableBase
     {
-        private int _count;
+        private decimal _count;
 
-        public int Count
+        public decimal Count
         {
             get { return _count; }
             set { SetProperty(ref _count, value); }
@@ -26,20 +26,75 @@ namespace App9
             set { SetProperty(ref _message, value); }
         }
 
-        public DelegateCommand ClickCommand { get; set; }
+        public DelegateCommand<object> ClickCommand { get; set; }
 
         public MainPageViewModel()
         {
-            ClickCommand = new DelegateCommand(ClickAction);
-            Message = "Hello, World!\nCount = " + Count.ToString();
+            ClickCommand = new DelegateCommand<object>(ClickAction);
+            Message = "0";  //"Hello, World!\nCount = " + Count.ToString();
             Count = 0;
         }
 
-        private void ClickAction()
+        private void ClickAction(object parameter)
         {
-            Random rnd = new Random();
-            Count += (int)((double)100000 * rnd.Next() / int.MaxValue);
-            Message = "Hello, Windows IoT!\nCount = " + Count.ToString();
+            //Random rnd = new Random();
+            //Count += (int)((double)100000 * rnd.Next() / int.MaxValue);
+            //Message = "Hello, Windows IoT!\nCount = " + Count.ToString();
+
+            string entry = (string)parameter;
+
+            switch (entry)
+            {
+                case "<":
+                    if (Message.Length > 0)
+                    {
+                        Message = Message.Substring(0, Message.Length - 1);
+                    }
+                    if (Message.Length == 0)
+                    {
+                        Message = "0";
+                    }
+                    break;
+
+                case null:
+                    break;
+
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (Message == "0" && entry != "0")
+                    {
+                        Message = entry;
+                    }
+                    else
+                    {
+                        Message = Message + entry;
+                    }
+                    break;
+
+                case ".":
+                    if(Message == "0")
+                    {
+                        Message = "0.";
+                    }
+                    else
+                    {
+                        if (!Message.Contains("."))
+                        {
+                            Message = Message + ".";
+                        }
+                    }
+                    break;
+            }
+
+            Count = decimal.Parse(Message);
         }
     }
 }
